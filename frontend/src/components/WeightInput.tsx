984315a558795246d510f7a0f65abf846bc4d611
paultreanor <treanorpaul9@gmail.com>
2025-05-01 19:55:33 +0100

    Add weight input section to card to make styling more consistent

--- frontend/src/components/WeightInput.tsx
+++ frontend/src/components/WeightInput.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
 type WeightInputProps = {
     onAddWeight: (weight: number) => void
@@ -16,20 +17,31 @@ export default function WeightInput({ onAddWeight}: WeightInputProps) {
     }
 
     return (
-        <form onSubmit={handleSubmit} className="mb-8 flex items-center justify-center space-x-2">
-            <label htmlFor="weight" className="text-lg font-medium font-varela-round">
-                What is your weight today?
-            </label>
-            <Input
-                type="number"
-                id="weight"
-                value={weight}
-                onChange={(e) => setWeight(e.target.value)}
-                className="w-24"
-                step="0.1"
-                required
-            />
-            <Button type="submit" className="font-varela-round">Add Weight</Button>
-        </form>
+        <Card className="w-full max-w-[850px] mx-auto mb-8">
+			<CardHeader className="px-4 sm:px-6">
+				<div className="flex items-center justify-between">
+					<CardTitle className="font-varela-round">Today's Weight</CardTitle>
+				</div>
+            </CardHeader>
+            <CardContent className="px-4 sm:px-6">
+                <form onSubmit={handleSubmit} className="mb-6">
+                    <label htmlFor="weight" className="font-varela-round">
+                        Your weight in kg or lbs
+                    </label>
+                    <div className="pt-4 flex space-x-2">
+                        <Input
+                            type="number"
+                            id="weight"
+                            value={weight}
+                            onChange={(e) => setWeight(e.target.value)}
+                            className="w-24"
+                            step="0.1"
+                            required
+                        />
+                        <Button type="submit" className="font-varela-round">Add Weight</Button>
+                        </div>
+                </form>
+            </CardContent>
+        </Card>
     )
 }
